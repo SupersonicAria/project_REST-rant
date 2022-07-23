42787986da11558bd1de3fba489f0ebbc43087e5
unknown <tavoywalls@gmail.com>
2022-07-23 13:41:42 -0400

    adding and rendering a new view

--- views/home.jsx
+++ views/home.jsx
@@ -5,10 +5,10 @@ function home() {
     return (
         <Def>
             <main>
-                <h1>HOME</h1>
-                <div>
+                <h1>REST-rant</h1>
+                <div id='box'>
                     <img id='food' src='/images/yum.jpg' alt='Good Food' />
-                    <div>
+                    <div id="box">
                         Photo by <a href="https://unsplash.com/@lvnatikk?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Lily Banse</a> on <a href="https://unsplash.com/s/photos/food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
                     </div>
                 </div>
